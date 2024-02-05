47c9b47c3d9a440c67eaca24dd3802320d3f4731
Suzy Vaque <79302468+suzyvaque@users.noreply.github.com>
2024-02-05 23:53:00 +0900

    Update Transformer-PositionalEncoding.py

--- machine-learning/Transformer-PositionalEncoding.py
+++ machine-learning/Transformer-PositionalEncoding.py
@@ -1,16 +1,20 @@
 '''
-# original torch-based code from jadore801120/attention-is-all-you-need-pytorch/transformer/Models.py
-# pytorch converted to tensorflow
-# class PositionalEncoding only, independently applicable before model training
+original torch-based code from jadore801120/attention-is-all-you-need-pytorch/transformer/Models.py
 
-# (x = batch size) number of (y = sequence length, z = hidden dim)
-# equals x number of sentences, where each sentence has y words, and each word is encoded to a z sized vector
-# apply positional encoding for each (y, z) shaped input, total x times
+1. pytorch converted to tensorflow
+2. class PositionalEncoding only, independently applicable before model training
+
+(x = batch size) number of (y = sequence length, z = hidden dim)
+equals x number of sentences, where each sentence has y words, and each word is encoded to a z sized vector
+
+Apply positional encoding for each (y, z) shaped input, total x times
+
+Refer to https://www.blossominkyung.com/deeplearning/transfomer-positional-encoding for sinusodial encoding
 '''
 
 
 
-# should be ipynb
+# Should be ipynb
 
 import tensorflow as tf
 import numpy as np
