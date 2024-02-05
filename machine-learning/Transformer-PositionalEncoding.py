e806e0f9baa3f326a9fd2dc8f516697204f483d5
Suzy Vaque <79302468+suzyvaque@users.noreply.github.com>
2024-02-05 23:09:34 +0900

    Create Transformer-PositionalEncoding.py

---
+++ machine-learning/Transformer-PositionalEncoding.py
@@ -0,0 +1,4 @@
+# jadore801120/attention-is-all-you-need-pytorch/transformer/Models.py
+# pytorch converted to tensorflow
+# class PositionalEncoding only, independently applicable before model training
+
