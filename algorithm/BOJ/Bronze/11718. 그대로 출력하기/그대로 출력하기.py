699742a799a8b667d526ba2c49bb4f587ba5769f
suzy <79302468+suzyvaque@users.noreply.github.com>
2024-02-28 01:15:58 +0900

    questionable comment

--- algorithm/BOJ/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ algorithm/BOJ/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -3,3 +3,15 @@ while True:
         print(input())
     except:
         break
+
+'''
+Fails with below (Why is it?)
+
+import sys
+
+while True:
+    try:
+        print(sys.stdin.readline().rstrip())
+    except:
+        break
+'''
